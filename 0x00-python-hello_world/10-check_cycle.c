210087b24cf5f748bc4232d6022f8b278d21e98d
Unarine <UnarineMunyai28@gmail.com>
2023-11-27 14:24:00 -0800

    linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,53 +1,25 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * main - check the code
+ * check_cycle - checks if a linked list contains a cycle
  *
- * Return: Always 0.
+ * Return: 1 if the list has a cycle, 0 if it doesn't
  */
-int main(void)
+int check_cycle(listint_t *list)
 {
-    listint_t *head;
-    listint_t *current;
-    listint_t *temp;
-    int i;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-    head = NULL;
-    add_nodeint(&head, 0);
-    add_nodeint(&head, 1);
-    add_nodeint(&head, 2);
-    add_nodeint(&head, 3);
-    add_nodeint(&head, 4);
-    add_nodeint(&head, 98);
-    add_nodeint(&head, 402);
-    add_nodeint(&head, 1024);
-    print_listint(head);
+	if (!list)
+		return (0);
 
-    if (check_cycle(head) == 0)
-        printf("Linked list has no cycle\n");
-    else if (check_cycle(head) == 1)
-        printf("Linked list has a cycle\n");
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
 
-    current = head;
-    for (i = 0; i < 4; i++)
-        current = current->next;
-    temp = current->next;
-    current->next = head;
-
-    if (check_cycle(head) == 0)
-        printf("Linked list has no cycle\n");
-    else if (check_cycle(head) == 1)
-        printf("Linked list has a cycle\n");
-
-    current = head;
-    for (i = 0; i < 4; i++)
-        current = current->next;
-    current->next = temp;
-
-    free_listint(head);
-
-    return (0);
+	return (0);
 }
